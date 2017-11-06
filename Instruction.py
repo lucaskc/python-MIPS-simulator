33445ec48a154f5878cbb303481fbfd8695f9e6b
Leonardo Cesar <guy.fawkes.05111605.e@gmail.com>
2017-11-06 17:20:41 -0200

    instruction.py modified to only support the basic instruction set

--- Instruction.py
+++ Instruction.py
@@ -38,62 +38,6 @@ class Instruction(object):
             else:
                 self.controls[key] = input[key]
     
-    @property
-    def op(self):
-        """ Get this Instruction's name """
-        return self.values['op']
-    
-    @property
-    def dest(self):
-        """ Get this Instruction's destination register """
-        return self.values['dest']
-    
-    @property
-    def s1(self):
-        """ Get this Instruction's first source register """
-        return self.values['s1']
-    
-    @property
-    def s2(self):
-        """ Get this Instruction's second source register """
-        return self.values['s2']
-    
-    @property
-    def immed(self):
-        """ Get this Instruction's immediate value """
-        return self.values['immed']
-    
-    @property
-    def target(self):
-        """ Get this Instruction's target value """
-        return self.values['target']
-    
-    @property
-    def aluop(self):
-        """ Get this Instruction's control to decide an alu operation """
-        return self.controls['aluop']
-    
-    @property
-    def regRead(self):
-        """ Get this Instruction's control to decide to read a register"""
-        return self.controls['regRead']
-    
-    @property
-    def regWrite(self):
-        """ Get this Instruction's control to decide to write a register """
-        return self.controls['regWrite']
-    
-    @property
-    def readMem(self):
-        """ Get this Instruction's control to decide to read memory """
-        return self.controls['readMem']
-    
-    @property
-    def writeMem(self):
-        """ Get this Instruction's control to decide to write memory """
-        return self.controls['writeMem']
-    
-    
     def __str__(self):
         str = "%s\t%s %s %s %s %s" % (self.values['op'],
                                   self.values['dest'] if self.values['dest'] else "",
@@ -114,9 +58,8 @@ Nop = Nop()
 class InstructionParser(object):
     def __init__(self):
         self.instructionSet = {
-            'rtype': ['add', 'sub', 'and', 'or', 'jr', 'nor', 'slt'],
-            'itype': ['addi', 'subi', 'ori', 'bne', 'beq', 'lw', 'sw'],
-            'jtype': ['j']
+            'rtype': ['add', 'sub', 'and', 'or'],
+            'itype': ['bne', 'lw', 'sw']
         }    
 
     def parseFile(self, filename):
@@ -135,15 +78,11 @@ class InstructionParser(object):
             return self.createRTypeInstruction(s)
         elif instr in self.instructionSet['itype']:
             return self.createITypeInstruction(s)    
-        elif instr in self.instructionSet['jtype']:
-            return self.createJTypeInstruction(s)
         else:
             raise ParseError("Invalid parse instruction")
 
     #TODO should be figuring out controls dynamically based on the op
     def createRTypeInstruction(self, s):
-        if s[0] == "jr":
-            return Instruction(op=s[0], s1 = s[1], regRead = 1, aluop=1)
         return Instruction(op=s[0], dest=s[1], s1=s[2], s2=s[3], regRead=1, regWrite=1, aluop=1)
 
     def createITypeInstruction(self, s):
@@ -161,12 +100,11 @@ class InstructionParser(object):
             else :
                 return Instruction(op=s[0],  s1 = s[1], s2=sval,immed = immedval, regRead = 1, aluop=1, writeMem = 1)
 
-        if ( s[0] == 'bne' or s[0] == 'beq') :
+        if (s[0] == 'bne'):
             return Instruction(op=s[0], s1=s[1] , s2= s[2], immed = s[3], regRead = 1, aluop = 1)
+
         return Instruction(op=s[0], dest=s[1], s1=s[2], immed=s[3], regRead=1, regWrite=1, aluop=1)
 
-    def createJTypeInstruction(self, s):
-        return Instruction(op=s[0], target=s[1])
 
 class ParseError(Exception):
     def __init__(self, value):
