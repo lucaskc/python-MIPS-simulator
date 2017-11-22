a5a918ff570e728e83bbc31e7e55d7454fa4267b
Sabrina <sabrina.tridico@usp.br>
2017-11-22 00:25:53 -0200

    Button and label of skeleton working! Yay!

--- interface.py
+++ interface.py
@@ -5,33 +5,21 @@ root = Tk()
 height = 5
 width = 5
 
-#Create a 5x5 excel-like table
 
+#Defining my methods before the elements need them
+def nextClock():
+    print("Here will be the code changing the registers")
+
+#Create a 5x5 excel-like table
 for i in range(height): #Rows
-    for j in range(width): #Columns
+    lbl = Label(root, text="OLÁ MUNDO")
+    lbl.grid(row=i, column=0)
+
+    for j in range(1, width + 1): #Columns
         b = Entry(root, text="")
         b.grid(row=i, column=j)
 
-nextClockBtn = Button(root, fg="red", text="Next Clock", command="nextClock")
-nextClockBtn.pack()
-
-#class Application(Frame):
-#    def __init__(self, master=None):
-#        super().__init__(master)
-#        self.pack()
-#        self.create_widgets()
-
-#    def create_widgets(self):
-#        self.nextClockBtn = tk.Button(self)
-#        self.nextClockBtn["text"] = "Next Clock"
-#        self.nextClockBtn["command"] = self.nextClock
-#        self.nextClockBtn.pack(side="top")
-
-#        self.quit = tk.Button(self, text="QUIT", fg="red",
-#                              command=root.destroy)
-#        self.quit.pack(side="bottom")
-
-def nextClock():
-    print("Here will be the code changing the registers")
+button = Button(root, text="É isso?", command=nextClock)
+button.grid(row=4, column=4)
 
 mainloop()
